4ac959d5918e131e0f138b352c2bd1cf3a01ec34
Nasser Sanou <nassersanou23@gmail.com>
2018-08-19 22:34:26 +0000

    automatically backed up by learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,10 +1,11 @@
 def using_while
-levitation_force = 0
-while	levitation_force < 10 
-	puts "Wingardium Leviosa"
-	levitation_force += 1
-end 
+  levitation_force = 0
+  while	levitation_force < 10 
+	  puts "Wingardium Leviosa"
+	  levitation_force += 1
+   end 
 
 end
 
+using_while
 
